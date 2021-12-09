ca250e671fce9369439e902d27ebc6bcc18d7b7c
David Byron <dbyron@salesforce.com>
2021-12-08 17:21:39 -0800

    feat(web): Generic exception handler support for `IllegalArgumentException` (#831) (#28)
    Replaces a one-off `ExceptionHandler` in `clouddriver`.
    
    @W-10285280
    
    Co-authored-by: Adam Jordens <adam@jordens.org>

--- kork-web/src/main/java/com/netflix/spinnaker/kork/web/exceptions/GenericExceptionHandlers.java
+++ kork-web/src/main/java/com/netflix/spinnaker/kork/web/exceptions/GenericExceptionHandlers.java
@@ -66,7 +66,11 @@ public class GenericExceptionHandlers extends BaseExceptionHandlers {
         HttpStatus.NOT_FOUND.value(), exceptionMessageDecorator.decorate(e, e.getMessage()));
   }
 
-  @ExceptionHandler({InvalidRequestException.class, UserException.class})
+  @ExceptionHandler({
+    InvalidRequestException.class,
+    UserException.class,
+    IllegalArgumentException.class
+  })
   public void handleInvalidRequestException(
       Exception e, HttpServletResponse response, HttpServletRequest request) throws IOException {
     storeException(request, response, e);
--- kork-web/src/test/groovy/com/netflix/spinnaker/kork/web/exceptions/GenericExceptionHandlersMvcSpec.groovy
+++ kork-web/src/test/groovy/com/netflix/spinnaker/kork/web/exceptions/GenericExceptionHandlersMvcSpec.groovy
@@ -76,6 +76,14 @@ class GenericExceptionHandlersMvcSpec extends Specification {
     entity.statusCode == HttpStatus.BAD_REQUEST
   }
 
+  def "should map IllegalArgumentException as 400"() {
+    when:
+    def entity = restTemplate.getForEntity("http://localhost:$port/test-controller/illegalArgumentException", HashMap.class)
+
+    then: "status is 400"
+    entity.statusCode == HttpStatus.BAD_REQUEST
+  }
+
   def "should handle IllegalStateException"() {
     when:
     def entity = restTemplate.getForEntity("http://localhost:$port/test-controller/illegalStateException", HashMap.class)
@@ -127,6 +135,11 @@ class GenericExceptionHandlersMvcSpec extends Specification {
 
     @GetMapping("/path1")
     void get(@RequestParam("param") String param) {
+    }
+
+    @GetMapping("/illegalArgumentException")
+    void illegalArgumentException() {
+      throw new IllegalArgumentException()
     }
 
     @GetMapping("/illegalStateException")
